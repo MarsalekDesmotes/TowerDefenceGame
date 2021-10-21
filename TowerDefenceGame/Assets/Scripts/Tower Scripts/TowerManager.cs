7893b033f912b51986198b6e0289ea42f6be9e39
MarsalekDesmotes <57544847+MarsalekDesmotes@users.noreply.github.com>
2021-10-21 19:22:40 +0300

    1.0

---
+++ TowerDefenceGame/Assets/Scripts/Tower Scripts/TowerManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+
+    public GameObject gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("carpti");
+        if (collision.gameObject.CompareTag("ajan"))
+        {
+           // Destroy(collision.gameObject);
+            Debug.Log("carpisma basarili");
+            gameController.GetComponent<GameController>().decreaseHP(0.1f);
+            GameController.health -= 1f;
+            //  Destroy(collision.gameObject);
+
+        }
+        if (collision.gameObject.CompareTag("ucak"))
+        {
+
+            gameController.GetComponent<GameController>().decreaseHP(0.3f);
+            GameController.health -= 3f;
+          //  Destroy(collision.gameObject);
+
+        }
+        if (collision.gameObject.CompareTag("tank"))
+        {
+
+            gameController.GetComponent<GameController>().decreaseHP(0.2f);
+            GameController.health -= 2f;
+           // Destroy(collision.gameObject);
+        }
+    }
+
+
+
+
+}
